573ad3aaaf9d8c5a4af85207ddc37a4ffffce6a2
Muhib_ur_Rehman <mrehman@nisum.com>
2021-07-29 19:55:39 +0500

    Account Structure

---
+++ src/com/company/CreditAccount.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class CreditAccount extends Accounts{
+    public ArrayList<VisaAccount> visaAccountArrayList;
+    public ArrayList<MasterAccount> masterAccountArrayList;
+}
